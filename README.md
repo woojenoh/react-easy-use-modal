8b79e55c657e3c788bcb799ea264a275ecaf100a
노우제 <woojenoh@gmail.com>
2022-07-11 22:42:05 +0900

    Update readme & configs

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-simple-use-modal
+
+## Installation
+
+```bash
+$ npm install --save react-simple-use-modal
+$ yarn add react-simple-use-modal
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-import progress from 'rollup-plugin-progress';
 import cleaner from 'rollup-plugin-cleaner';
-import copy from 'rollup-plugin-copy';
+import progress from 'rollup-plugin-progress';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.ts',
@@ -24,20 +25,37 @@ export default {
     },
   ],
   plugins: [
-    progress(),
     cleaner({
       targets: ['build'],
     }),
-    copy({
-      targets: [
-        { src: 'package.json', dest: 'build' },
-      ],
-    }),
+    progress(),
     commonjs(),
     resolve(),
     peerDepsExternal(),
     babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
     typescript(),
     terser(),
+    generatePackageJson({
+      outputFolder: 'build',
+      baseContents: (pkg) => ({
+        name: pkg.name,
+        version: pkg.version,
+        description: pkg.description,
+        main: 'cjs/src/index.js',
+        module: 'esm/src/index.js',
+        types: 'esm/index.d.ts',
+        repository: pkg.repository,
+        author: pkg.author,
+        license: pkg.license,
+        bugs: pkg.bugs,
+        homepage: pkg.homepage,
+        peerDependencies: pkg.peerDependencies,
+      }),
+    }),
+    copy({
+      targets: [
+        { src: 'README.md', dest: 'build' },
+      ],
+    }),
   ],
 };
